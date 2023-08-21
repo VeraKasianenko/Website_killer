0d698f969943e6fd017a234e9216ed3907905825
Pelmeshka <112972833+VeraKasianenko@users.noreply.github.com>
2023-08-21 22:09:57 +0300

    data registration

--- Website_killer_Java/src/main/java/Website_killer/Main.java
+++ Website_killer_Java/src/main/java/Website_killer/Main.java
@@ -4,63 +4,68 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import javax.swing.*;
-import java.io.File;
-import java.io.RandomAccessFile;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            File lockFile = new File("app.lock");
-            RandomAccessFile randomAccessFile = new RandomAccessFile(lockFile, "rw");
-            FileChannel channel = randomAccessFile.getChannel();
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException, InterruptedException, IOException {
+        // Путь к файлу date.txt
+        String filePath = "date_web.txt";
 
-            FileLock lock = channel.tryLock();
-            if (lock == null) {
-                // Если блокировку не удалось получить, приложение уже запущено
-                JOptionPane.showMessageDialog(null, "Доступ на сегодня закончился", "Ошибка", JOptionPane.ERROR_MESSAGE);
-                channel.close();
-                randomAccessFile.close();
-                System.exit(1);
-            }
+        // Проверяем, существует ли файл
+        File file = new File(filePath);
+        boolean fileExists = file.exists();
 
-            // Время для таймера (40 минут)
-            int time = 40 * 60 * 1000;
+        // Если файл не существует, создаем его и записываем текущую дату
+        if (fileExists) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String storedDate = reader.readLine();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String currentDate = dateFormat.format(new Date());
+                if (currentDate.equals(storedDate)) {
+                    JOptionPane.showMessageDialog(null, "На сегодня доступ запрещен", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                    System.exit(0);
 
-            // Целевой домен, который нужно закрыть
-            String targetDomain = "https://yandex.ru/games";
-
-            // Устанавливаем путь к драйверу Firefox
-            System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Mozilla Firefox\\geckodriver.exe");
-
-            // Создаем экземпляр веб-драйвера Firefox
-            WebDriver driver = new FirefoxDriver();
-
-            // Переходим на целевой домен
-            driver.navigate().to(targetDomain);
-
-            // Таймер
-            Thread.sleep(time);
-
-            // Проверка всех вкладок, содержат ли они целевой домен, если да, то они будут закрыты
-            for (String handle : driver.getWindowHandles()) {
-                driver.switchTo().window(handle);
-                if (driver.getCurrentUrl().contains(targetDomain)) {
-                    driver.close();
                 }
             }
-
-            // Вывод сообщения об окончании игровой сессии
-            UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-            JOptionPane optionPane = new JOptionPane("Время работы игры истекло", JOptionPane.INFORMATION_MESSAGE);
-            JDialog dialog = optionPane.createDialog(frame, "Сообщение");
-            dialog.setIconImage(null);
-            dialog.setVisible(true);
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "На сегодня доступ запрещен", "Ошибка", JOptionPane.ERROR_MESSAGE);
         }
+
+        try (PrintWriter writer = new PrintWriter(filePath)) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String currentDate = dateFormat.format(new Date());
+            writer.println(currentDate);
+        }
+
+        // Время для таймера (20 минут)
+        int time = 20 * 60 * 1000;
+
+        // Целевой домен, который нужно закрыть
+        String targetDomain = "https://yandex.ru/games";
+
+        // Устанавливаем путь к драйверу Firefox
+        System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Mozilla Firefox\\geckodriver.exe");
+
+        // Создаем экземпляр веб-драйвера Firefox
+        WebDriver driver = new FirefoxDriver();
+
+        // Переходим на целевой домен
+        driver.navigate().to(targetDomain);
+
+        // Таймер
+        Thread.sleep(time);
+
+        // Проверка всех вкладок, содержат ли они целевой домен, если да, то они будут закрыты
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+            if (driver.getCurrentUrl().contains(targetDomain)) {
+                driver.close();
+            }
+        }
+
+        // Вывод сообщения об окончании игровой сессии
+        UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        JOptionPane.showMessageDialog(null, "Время работы игры истекло", "Сообщение", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
     }
 }
